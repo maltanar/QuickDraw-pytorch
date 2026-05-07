97bc3969c27f2ed5bd13fef7117195b308300de7
Yaman Umuroglu <maltanar@gmail.com>
2026-05-07 15:14:14 +0200

    remove checkpoints, update experiments script

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# Ensure we're in the right directory
-cd /data/sandbox/QuickDraw-pytorch
-
 echo "Preparing data..."
-python DataUtils/prepare_data.py --download 0 --categories 10 -v 0.2
+python DataUtils/prepare_data.py --download 1 --categories 10 -v 0.2
 
 echo "Running 8-bit QAT..."
 python main_qat.py --bit_width 8 --epochs 15 --export_qonnx
 
-echo "Running 4-bit QAT..."
-python main_qat.py --bit_width 4 --epochs 25 --export_qonnx
+#echo "Running 4-bit QAT..."
+#python main_qat.py --bit_width 4 --epochs 25 --export_qonnx --no_narrow_range --ngpu 0
 
-echo "Running 2-bit QAT..."
-python main_qat.py --bit_width 2 --epochs 100 --export_qonnx
+#echo "Running 2-bit QAT..."
+#python main_qat.py --bit_width 2 --epochs 100 --export_qonnx
 
 echo "All experiments complete."
